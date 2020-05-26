8c9f6616ac16ebc80cd69f342cd87f1b6e1e6abf
György Dóra <gyorgydora99@gmail.com>
2020-05-26 11:35:45 +0200

    Added packages and classes

---
+++ src/main/java/sudoku/results/GameResult.java
@@ -0,0 +1,46 @@
+package sudoku.result;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class GameResult {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+
+    @Column(nullable = false)
+    private String player;
+
+
+    private boolean solved;
+
+
+    private int steps;
+
+
+    @Column(nullable = false)
+    private Duration duration;
+
+
+    @Column(nullable = false)
+    private ZonedDateTime created;
+
+    @PrePersist
+    protected void onPersist() {
+        created = ZonedDateTime.now();
+    }
+
+}
---
+++ src/main/java/sudoku/state/SudokuState.java
@@ -0,0 +1,112 @@
+package sudoku.state;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@Slf4j
+public class SudokuState {
+
+    public int[][] initial;
+
+    public int[][] solution;
+
+    public int[][] gameBoard;
+
+    public SudokuState() {
+
+        initial = new int[][] {
+                {0, 0, 0, 4, 0, 0, 0, 9, 0},
+                {6, 0, 7, 0, 0, 0, 8, 0, 4},
+                {0, 1, 0, 7, 0, 9, 0, 0, 3},
+                {9, 0, 1, 0, 7, 0, 0, 3, 0},
+                {0, 0, 2, 0, 0, 0, 9, 0, 0},
+                {0, 5, 0, 0, 4, 0, 1, 0, 7},
+                {3, 0, 0, 5, 0, 2, 0, 7, 0},
+                {4, 0, 6, 0, 0, 0, 3, 0, 1},
+                {0, 7, 0, 0, 0, 4, 0, 0, 0}
+        };
+
+        solution = new int[][] {
+                {5, 3, 8, 4, 6, 1, 7, 9, 2},
+                {6, 9, 7, 3, 2, 5, 8, 1, 4},
+                {2, 1, 4, 7, 8, 9, 5, 6, 3},
+                {9, 4, 1, 2, 7, 8, 6, 3, 5},
+                {7, 6, 2, 1, 5, 3, 9, 4, 8},
+                {8, 5, 3, 9, 4, 6, 1, 2, 7},
+                {3, 8, 9, 5, 1, 2, 4, 7, 6},
+                {4, 2, 6, 8, 9, 7, 3, 5, 1},
+                {1, 7, 5, 6, 3, 4, 2, 8, 9}
+        };
+
+        gameBoard = new int[9][9];
+    }
+
+    public int[][] getInitial() {
+        return initial;
+    }
+
+    public int[][] getSolution() {
+        return solution;
+    }
+
+    public int[][] getGameBoard() {
+        return gameBoard;
+    }
+
+    public void modifyGameBoard(int num, int row, int col) {
+        if (initial[row][col] == 0) {
+            if (num >=0 && num <= 9) {
+                gameBoard[row][col] = num;
+            }
+            else {
+                //log
+                System.out.println("Value passed to game board is wrong");
+            }
+        }
+    }
+
+    public boolean checkForSolution() {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (initial[row][col] == 0) {
+                    if (gameBoard[row][col] != solution[row][col]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+
+    public boolean checkForRules() {
+
+        int[][] full = new int[9][9];
+        int[] included;
+        int sum = 0;
+
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (initial[row][col] == 0) {
+                    full[row][col] = gameBoard[row][col];
+                } else {
+                    full[row][col] = initial[row][col];
+                }
+            }
+        }
+
+        for (int row = 0; row < 9; row++) {
+            sum = 0;
+            included = new int[9];
+            for (int col = 0; col < 9; col++) {
+                sum = sum + full[row][col];
+                included[full[row][col]-1]++;
+            }
+        }
+
+
+        return true;
+    }
+
+}
